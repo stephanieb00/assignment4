931e60cd9c28c7966634d41caf1f459e0abb61ab
Stephanie <stephanieb@tywls-astoria.org>
2021-06-22 14:15:52 -0400

    Created UserProfile component and linked to home route

--- bank-of-react/src/App.js
+++ bank-of-react/src/App.js
@@ -1,26 +1,41 @@
+// src/App.js
+
 import React, {Component} from 'react';
-import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+import {BrowserRouter as Router, Route} from 'react-router-dom';
 import Home from './components/Home';
+import UserProfile from './components/UserProfile';
 
 class App extends Component {
-  
-  render() {
-    const HomeComponent = () => (<Home accountBalance={this.state.accountBalance}/>);
-    return (
-        <Router>
-          <Switch>
-            <Route exact path="/" render={HomeComponent}/>
-          </Switch>
-        </Router>
-    );
-  }
+
   constructor() {
     super();
 
     this.state = {
-      accountBalance: 14568.27
+      accountBalance: 14568.27,
+      currentUser: {
+        userName: 'joe_shmo',
+        memberSince: '07/23/96',
+      }
     }
   }
+
+  render() {
+
+    const HomeComponent = () => (<Home accountBalance={this.state.accountBalance}/>);
+    const UserProfileComponent = () => (
+        <UserProfile userName={this.state.currentUser.userName} memberSince={this.state.currentUser.memberSince}  />
+    );
+
+    return (
+        <Router>
+          <div>
+            <Route exact path="/" render={HomeComponent}/>
+            <Route exact path="/userProfile" render={UserProfileComponent}/>
+          </div>
+        </Router>
+    );
+  }
+
 }
 
 export default App;
