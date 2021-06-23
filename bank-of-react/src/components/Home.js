ee1b7391f3296055bad83ca8a92d1953a7b4ecf7
Stephanie <stephanieb@tywls-astoria.org>
2021-06-22 20:06:31 -0400

    Coded for the credit component and added elements into app.js

--- bank-of-react/src/components/Home.js
+++ bank-of-react/src/components/Home.js
@@ -10,10 +10,12 @@ class Home extends Component {
         <div>
           <img src="https://letstalkpayments.com/wp-content/uploads/2016/04/Bank.png" alt="bank"/>
           <h1>Bank of React</h1>
+          <AccountBalance accountBalance = {this.props.accountBalance}/>
 
           <Link to="/userProfile">User Profile</Link>
-
-          <AccountBalance accountBalance={this.props.accountBalance}/>
+          <Link to="/login">Login</Link>
+          <Link to="/credits">Credits</Link>
+          <Link to="/debits">Debits</Link>
         </div>
     );
   }
